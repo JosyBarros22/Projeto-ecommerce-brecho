704bde8dde5845f2841c0f28dadb2906630fae84
JosyBarros22 <103906518+JosyBarros22@users.noreply.github.com>
2023-01-14 15:36:34 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { GlobalStyle } from './GlobalStyle';
+import Home from './pages/Home/Home';
+
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle/>
+      <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
